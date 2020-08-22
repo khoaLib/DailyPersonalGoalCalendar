c811e94d01db57c87249193430b4f492e7d02063
khoaLib <phamkhoa996@gmail.com>
2020-08-21 17:11:38 -0700

    finish light/dark mode but need to fix 2 images

--- index.js
+++ index.js
@@ -9,9 +9,7 @@
 // }
 
 
-document.querySelector("img").addEventListener("click", function() {
-  alert("first Image got clicked")
-});
+document.querySelector("img").addEventListener("click", function() {});
 $(document).ready(function() {
   var flag = false;
 
@@ -19,24 +17,46 @@ $(document).ready(function() {
     flag = !flag;
     if (flag) {
       changeLightImage();
+      changeBlackBackground()
     } else {
       changeDarkImage();
+      changeWhiteBackground()
     }
   })
 })
 
+function changeBlackBackground() {
+  $("body").addClass("colorWhite");
+  $("div.text-box-left").removeClass("colorBorderBlack").addClass("colorBorderWhite");
+  $("div.text-box-right").removeClass("colorBorderBlack").addClass("colorBorderWhite");
+  $("div.center1").removeClass("colorBorderBlack").addClass("colorBorderWhite");
+  $("hr.topHr").removeClass("topHr").addClass("topHrWhite");
+  $("div.left-vertical").removeClass("left-vertical").addClass("left-vertical-white");
+  $("div.right-vertical").removeClass("right-vertical").addClass("right-vertical-white");
+}
+
+function changeWhiteBackground(){
+  $("body").removeClass("colorWhite");
+  $("div.text-box-left").removeClass("colorBorderWhite").addClass("colorBorderBlack");
+  $("div.text-box-right").removeClass("colorBorderWhite").addClass("colorBorderBlack");
+  $("div.center1").removeClass("colorBorderWhite").addClass("colorBorderBlack");
+  $("hr.topHrWhite").removeClass("topHrWhite").addClass("topHr");
+  $("div.left-vertical-white").removeClass("left-vertical-white").addClass("left-vertical");
+  $("div.right-vertical-white").removeClass("right-vertical-white").addClass("right-vertical");
+}
+
 function changeLightImage() {
   alert("Go dark mode");
-  $("img.sunMoon").attr("src", "images\\whiteSunMoon.png");
+  $("img.sunMoon").attr("src", "images\\moonDay.png");
   $("img.darkMode").attr("src", "images\\lightMode.png");
-  $("img.symbolDark").attr("src", "images\\symBolLight.png");
-  $("img.userDark").attr("src", "images\\user.png");
+  $("img.symbolDark").attr("src", "images\\symbolDark.png");
+  $("img.userDark").attr("src", "images\\download.png");
 }
 
 function changeDarkImage() {
   alert("Go Light mode");
-  $("img.sunMoon").attr("src", "images\\day-and-night.png");
+  $("img.sunMoon").attr("src", "images\\dayMoon.png");
   $("img.darkMode").attr("src", "images\\darkMode.png");
-  $("img.symbolDark").attr("src", "images\\symbolDark.png");
-  $("img.userDark").attr("src", "images\\user(1).png");
+  $("img.symbolDark").attr("src", "images\\symbolLight.png");
+  $("img.userDark").attr("src", "images\\user (2).png");
 }
